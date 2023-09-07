8f0b0635b7c6467407ba7197f12b9e8e0324adeb
Adrian <dreamweaver3d@gmail.com>
2023-09-07 12:22:00 +0800

    dom tasks commit 4

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+    let hello = document.getElementById("hello");
+
+    fetch("https://hellosalut.stefanbohacek.dev/?lang=fr")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status}`);
+        }
+    
+        return response.json();
+      })
+      .then((json) => {
+        hello.textContent = json.hello;
+      })
+      .catch((err) => console.error(`Fetch problem: ${err.message}`));
+}
