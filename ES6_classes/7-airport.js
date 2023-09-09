577c6e944cbcc3174ff78c05d6caa59f13a9da6b
Adrian <dreamweaver3d@gmail.com>
2023-09-09 17:19:21 +0800

    es6 task 7 commit 1

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,6 +1,17 @@
+const util = require('util');
+
 export default class Airport {
-    constructor(name, code) {
-        this._name = name;
-        this._code = code;
-    }
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  [util.inspect.custom](depth, options) {
+    return `${this.constructor.name} [${this._code}] { _name: '${this._name}', _code: '${this._code}'}`;
+  }
+
+  toString() {
+    let to = typeof this;
+    return `[${to} ${this._code}]`;
+  }
 }
