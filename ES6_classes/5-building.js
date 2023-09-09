70346f093c42c9e147f91709a9368bd5cea1a11b
Adrian <dreamweaver3d@gmail.com>
2023-09-09 16:29:06 +0800

    es6 task 5 commit 2

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,7 +2,9 @@ export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
 
-    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+    if (this.constructor === Building) {
+      // Do the default behavior for Building class
+    } else if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
