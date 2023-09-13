70998c2f292cdcae29dc102a0dfd551563dbd92f
Adrian <dreamweaver3d@gmail.com>
2023-09-13 15:38:19 +0800

    scraping task 0 commit 1

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// console.log(process.argv);
+// console.log(process.argv[2]);
+
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
