440aa296b9b22c29e4f0311f76720862833fd0f0
Adrian <dreamweaver3d@gmail.com>
2023-09-14 16:57:23 +0800

    JSscraping task 2 commit 4

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -6,8 +6,7 @@ const url = process.argv[2];
 request(url, function (error, response, body) {
   if (error) {
     console.error('error:', error); // Print the error if one occurred
-  }
-  else {
+  } else {
     console.log('code:', response.statusCode);
   }
 });
