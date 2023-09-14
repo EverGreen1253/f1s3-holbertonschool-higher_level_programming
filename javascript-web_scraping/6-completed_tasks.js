68feb438289c1446765112ead39167507ccd8c79
Adrian <dreamweaver3d@gmail.com>
2023-09-14 18:13:04 +0800

    JSscraping task 6 commit 2

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -7,12 +7,12 @@ request(url, function (error, response, body) {
   if (error) {
     console.error('error:', error); // Print the error if one occurred
   } else {
-    let stats = new Object;
+    const stats = {};
 
     const data = JSON.parse(body);
-    for (let task of data) {
+    for (const task of data) {
       if (task.completed) {
-        const key = task.userId.toString()
+        const key = task.userId.toString();
         if (!stats[key]) {
           stats[key] = 0;
         }
