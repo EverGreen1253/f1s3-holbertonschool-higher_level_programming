b869ba3fc6437f99a9e5ed70430c64af56a54815
Adrian <dreamweaver3d@gmail.com>
2023-11-24 11:17:49 +0800

    api task 1 commit 2

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -40,8 +40,8 @@ for task in tasks_list:
 
 to_save = ""
 for item in all_tasks:
-    to_save = (to_save + "\"" + user_id + "\",\"" + name + "\",\"" +
-        str(item['completed']) + "\",\"" + item['title'] + "\"\n")
+    to_save = (to_save + "\"" + user_id + "\",\"" + name + "\",\"" + 
+               str(item['completed']) + "\",\"" + item['title'] + "\"\n")
 
 file_name = user_id + ".csv"
 with open(file_name, "w", encoding="utf-8") as f:
