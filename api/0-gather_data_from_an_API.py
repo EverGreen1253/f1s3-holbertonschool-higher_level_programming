a0d3c11f6dd342701ba06a50fa3e65169af75dc1
Adrian <dreamweaver3d@gmail.com>
2023-11-24 09:16:02 +0800

    api task 0 commit 1

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import sys
+import requests
+
+
+if len(sys.argv) == 1:
+    print("Please specify a user Id")
+    sys.exit()
+
+user_id = path = sys.argv[1]
+
+user_api_url = "https://jsonplaceholder.typicode.com/users/" + user_id
+todos_api_url = "https://jsonplaceholder.typicode.com/users/" + user_id + "/todos"
+
+todos = requests.get(url=todos_api_url, timeout=10)
+# print(todos.json())
+
+user = requests.get(url=user_api_url, timeout=10)
+employee = user.json()
+
+name = employee["name"]
+
+tasks_list = []
+count = 0
+completed = 0
+for item in todos.json():
+    if item['completed']:
+        tasks_list.append(item["title"])
+        completed = completed + 1
+    count  = count + 1
+
+print("Employee " + name + " is done with tasks(" + str(completed) + "/" + str(count) + "):")
+for task in tasks_list:
+    print("    " + task)
