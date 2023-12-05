a1bb961e07d455390633bd91e22f63bbd2d153fb
Adrian <dreamweaver3d@gmail.com>
2023-12-05 11:45:32 +0800

    Python annotations task 8 added Callable return type

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Nameless module for Task 8"""
+from typing import Callable
 
-
-def make_multiplier(multiplier: float):
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """returns function that multiplies"""
 
     def mult(v: float) -> float:
