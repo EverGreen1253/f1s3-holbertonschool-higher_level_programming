fe4e890d733517c8180c595d16ed132e3391b075
Adrian <dreamweaver3d@gmail.com>
2023-12-05 11:30:58 +0800

    Python annotations task 7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Nameless module for Task 7"""
+from typing import List, Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Output is stuffed into a list"""
+    output = (k, v)
+
+    return output
