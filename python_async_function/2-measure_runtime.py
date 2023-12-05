0404a5637ea01dc49f2ee65a7454edc3c58225b7
Adrian <dreamweaver3d@gmail.com>
2023-12-05 16:22:23 +0800

    python async task 2

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Nameless module for Python Async Task 2"""
+import asyncio
+from typing import List
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int = 10) -> float:
+    """returns average execution time per function call"""
+
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    return elapsed_time / n
