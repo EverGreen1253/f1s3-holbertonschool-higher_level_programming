1902a952c7b6dfd0fba09ed2d52d1903ff1de1de
Adrian <dreamweaver3d@gmail.com>
2023-12-06 07:52:50 +0800

    async comp task 1 added list return type

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 """Nameless module for Python Async Comp Task 0"""
-from random import random
-import asyncio
-from typing import Generator
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """Calls async_generator to collect 10 numbers"""
 
     result = []
