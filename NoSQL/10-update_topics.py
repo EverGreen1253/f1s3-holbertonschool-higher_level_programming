36a87cbccd1f87db8bf02e1fd56c5e74bba1a968
Adrian <dreamweaver3d@gmail.com>
2023-12-08 09:12:48 +0800

    nosql task 10 commit 2

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -8,6 +8,4 @@ def update_topics(mongo_collection, name, topics):
 
     mongo_collection.update_many(
         { 'name': name },
-        { '$set': { 'topics': topics }},
-        True
-        )
+        { '$set': { 'topics': topics }})
