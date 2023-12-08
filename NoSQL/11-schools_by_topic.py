1fffe35e57f4258bad2fdfab112f9f2d318e9207
Adrian <dreamweaver3d@gmail.com>
2023-12-08 09:32:26 +0800

    nosql task 11 commit 1

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Nameless module for task 10"""
+from pymongo import MongoClient
+
+
+def schools_by_topic(mongo_collection, topic):
+    """update topics in collection"""
+
+    output = []
+    for v in mongo_collection.find():
+        if 'topics' in v:
+            if 'Python' in v['topics']:
+                output.append(v)
+
+    return output
