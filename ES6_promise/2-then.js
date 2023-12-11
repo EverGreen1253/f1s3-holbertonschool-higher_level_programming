d6a1995ce7a5e92d51f7b96646c7ae4fc11e62a1
Adrian <dreamweaver3d@gmail.com>
2023-12-11 12:53:03 +0800

    Promises task 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,7 +2,7 @@ export default function handleResponseFromAPI(promise) {
   console.log('Got a response from the API');
 
   return promise
-    .then(() => ({
+    .then((resp) => resp.json({
       status: 200,
       body: 'Success',
     }))
