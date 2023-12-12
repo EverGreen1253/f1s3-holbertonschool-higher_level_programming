1d29a0f33137cb87fb3487d7450c61515705c732
Adrian <dreamweaver3d@gmail.com>
2023-12-12 10:56:26 +0800

    Promises Task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,26 +2,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const user = signUpUser(firstName, lastName);
-  const photo = uploadPhoto(fileName);
+  const user = signUpUser(firstName, lastName)
+    .then((value) => ({
+      status: 'fulfilled',
+      value,
+    }));
 
-  const output = [];
+  const photo = uploadPhoto(fileName)
+    .catch((e) => ({
+      status: 'rejected',
+      value: e.message,
+    }));
 
-  return user
-    .then((value) => {
-      output.push({
-        status: 'fulfilled',
-        value,
-      });
-
-      return photo;
-    })
-    .catch((e) => {
-      output.push({
-        status: 'rejected',
-        value: e.message,
-      });
-    }).finally(() => {
-      return output;
-    });
+  return Promise.all([user, photo]);
 }
