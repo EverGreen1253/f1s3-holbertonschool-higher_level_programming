431d5b0421b8a1a47a15fbaf49dbc5cc268cf7ce
Adrian <dreamweaver3d@gmail.com>
2023-12-18 16:21:12 +0800

    nodejs task 1 changed code  from readline to stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,17 +1,28 @@
-const { exit } = require('process');
-const readline = require('readline');
+// const { exit } = require('process');
+// const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// function ask(question) {
+//   rl.question(question, (answer) => {
+//     console.log(`Your name is: ${answer}`);
+//     console.log('This important software is now closing');
+//     exit();
+//   });
+// }
+
+// ask('Welcome to Holberton School, what is your name?\n');
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.resume();
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name}`);
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
 });
-
-function ask(question) {
-  rl.question(question, (answer) => {
-    console.log(`Your name is: ${answer}`);
-    console.log('This important software is now closing');
-    exit();
-  });
-}
-
-ask('Welcome to Holberton School, what is your name?\n');
