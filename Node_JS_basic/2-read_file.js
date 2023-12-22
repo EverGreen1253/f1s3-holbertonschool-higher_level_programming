20db61383804976855bd094d4f45c1d3ce19ea66
Adrian <dreamweaver3d@gmail.com>
2023-12-22 12:34:49 +0800

    nodejs task 2 - changed number of students to use .length

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -24,11 +24,8 @@ module.exports = function countStudents(filename) {
     // console.log(`Number of students in SWE: ${sweNames.length}. List: ${sweNames.join(', ')}`);
 
     const data = fs.readFileSync(filename, 'utf-8');
-    const headers = data.split('\n', 1)[0].split(',');
+    // const headers = data.split('\n', 1)[0].split(',');
     const noHeaderArray = data.split('\n').slice(1);
-
-    // strip headers and convert to list of dicts
-    const dictList = [];
 
     let countCS = 0;
     let countSWE = 0;
@@ -38,39 +35,23 @@ module.exports = function countStudents(filename) {
     for (const row of noHeaderArray) {
       const data = row.split(',');
 
-      if (data[3] === 'CS') {
+      if (data[3] === 'CS') { // hardcode
         countCS += 1;
         studentsCS.push(data[0]);
       }
 
-      if (data[3] === 'SWE') {
+      if (data[3] === 'SWE') { // hardcode
         countSWE += 1;
         studentsSWE.push(data[0]);
       }
-
-      const item = {
-        firstname: data[0], // hardcode
-        field: data[3], // hardcode
-      };
-      dictList.push(item);
     }
-
-    // dictList.forEach((element) => {
-    //   if (element.field === 'CS') {
-    //     countCS += 1;
-    //     studentsCS.push(element.firstname);
-    //   } else if (element.field === 'SWE') {
-    //     countSWE += 1;
-    //     studentsSWE.push(element.firstname);
-    //   }
-    // });
 
     const countStudents = countCS + countSWE;
 
     // print statements
     console.log(`Number of students: ${countStudents}`);
-    console.log(`Number of students in CS: ${countCS}. List: ${studentsCS.toString().split(',').join(', ')}`);
-    console.log(`Number of students in SWE: ${countSWE}. List: ${studentsSWE.toString().split(',').join(', ')}`);
+    console.log(`Number of students in CS: ${studentsCS.length}. List: ${studentsCS.toString().split(',').join(', ')}`);
+    console.log(`Number of students in SWE: ${studentsSWE.length}. List: ${studentsSWE.toString().split(',').join(', ')}`);
 
   } catch (error) {
     throw new Error('Cannot load the database');
