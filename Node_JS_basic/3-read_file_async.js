f609f8bf4fe96fc91693597aa9406cc18a168da9
Adrian <dreamweaver3d@gmail.com>
2023-12-22 12:51:04 +0800

    Nodejs Task 3 - added back commented out original code that didn't pass the checker

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -2,6 +2,29 @@ const fs = require('fs');
 
 module.exports = async function countStudents(filename) {
   try {
+    // const data = await fs.readFileSync(filename, 'utf-8');
+    // const rows = data.split('\n');
+    // let rowCount = 0;
+    // const sweNames = [];
+    // const csNames = [];
+
+    // for (const row of rows) {
+    //   rowCount += 1;
+
+    //   if (row.substring(row.length - 3) === 'SWE') {
+    //     sweNames.push(row.split(',')[0]);
+    //   }
+    //   if (row.substring(row.length - 2) === 'CS') {
+    //     csNames.push(row.split(',')[0]);
+    //   }
+    // }
+
+    // console.log(`Number of students: ${rowCount - 1}`);
+    // console.log(`Number of students in CS: ${csNames.length}. List: ${csNames.join(', ')}`);
+    // console.log(`Number of students in SWE: ${sweNames.length}. List: ${sweNames.join(', ')}`);
+
+    // ----- Old code that doesn't pass checker ends here ----
+
     const data = await fs.readFileSync(filename, 'utf-8');
     const rows = data.split('\n').slice(1);
 
