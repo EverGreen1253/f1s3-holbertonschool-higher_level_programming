4b0f52dd4a144803f41252b8b643e5f1b967436b
Adrian <dreamweaver3d@gmail.com>
2024-01-08 11:16:12 +0800

    nodejs task 4 fix 3

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,15 +1,16 @@
-const http = require("http");
+const http = require('http');
+
 const host = 'localhost';
 const port = 1245;
 
-const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end("Hello Holberton School!");
-};
+function requestListener(req, res) {
+  res.writeHead(200);
+  res.end('Hello Holberton School!');
+}
 
 const app = http.createServer(requestListener);
 app.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+  console.log(`Server is running on http://${host}:${port}`);
 });
 
 module.exports = app;
