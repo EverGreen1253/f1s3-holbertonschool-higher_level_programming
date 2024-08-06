272808140201de44e9d8c1f1fd7f6fcd740bf4a5
Adrian <dreamweaver3d@gmail.com>
2024-08-06 16:18:28 +0800

    pandas task 4 and 5 done

--- pandas/4-array.py
+++ pandas/4-array.py
@@ -5,9 +5,6 @@ from_file = __import__('2-from_file').from_file
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-# Start of my code
-df = df[["Timestamp", "Close"]]
-df = df.rename(columns={'Timestamp': 'Datetime'})
-# End of my code
+A = df[["High", "Close"]].tail(10).to_numpy()
 
-print(df.tail())
+print(A)
--- pandas/5-slice.py
+++ pandas/5-slice.py
@@ -5,9 +5,6 @@ from_file = __import__('2-from_file').from_file
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-# Start of my code
-df = df[["Timestamp", "Close"]]
-df = df.rename(columns={'Timestamp': 'Datetime'})
-# End of my code
+df = df.loc[::60, "High":"Volume_(BTC)"].tail()
 
 print(df.tail())
