963b97aa1b33737d4bb84294adaf03c17f6bcd7d
Adrian <dreamweaver3d@gmail.com>
2024-09-05 14:20:06 +0800

    trying a new way of doing the newline character that will hopefully pass the checker

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -7,31 +7,21 @@ export default class StudentsController {
 
     readDatabase(filename)
       .then((data) => {
-        // let outputString = '';
-        // outputString += 'This is the list of our students\n';
+        let outputString = '';
+        outputString += `This is the list of our students
+`;
 
-        // let students = data.CS;
-        // outputString += `Number of students in CS: ${students.length}. `;
-        // outputString += `List: ${students.toString()}\n`;
+        let students = data.CS;
+        outputString += `Number of students in CS: ${students.length}. `;
+        outputString += `List: ${students.join(', ')}
+`;
 
-        // students = data.SWE;
-        // outputString += `Number of students in SWE: ${students.length}. `;
-        // outputString += `List: ${students.toString()}\n`;
+        students = data.SWE;
+        outputString += `Number of students in SWE: ${students.length}. `;
+        outputString += `List: ${students.join(', ')}
+`;
 
-        // response.status(200).end(outputString);
-
-        // Apparently the way I did it does not play nice with the stupid checker's conditions
-        // Here is the solution they want
-        const students = [];
-        let message;
-        students.push('This is the list of our students');
-
-        for (const key of Object.keys(data)) {
-          message = `Number of students in ${key}: ${data[key].length}. List: ${data[key].join(', ')}`;
-          students.push(message);
-        }
-
-        response.status(200).send(`${students.join('\n')}`);
+        response.status(200).end(outputString);
       })
       .catch((error) => {
         response.status(500).end(error.message);
