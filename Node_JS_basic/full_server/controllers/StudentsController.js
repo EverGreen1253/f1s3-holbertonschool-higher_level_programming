955ee715bc9029d32f3a617d27bd23dde0afc78e
Adrian <dreamweaver3d@gmail.com>
2024-09-05 13:54:34 +0800

    updated studentcontroller output text format to match what checker wants

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -7,18 +7,31 @@ export default class StudentsController {
 
     readDatabase(filename)
       .then((data) => {
-        let outputString = '';
-        outputString += 'This is the list of our students\n';
+        // let outputString = '';
+        // outputString += 'This is the list of our students\n';
 
-        let students = data.CS;
-        outputString += `Number of students in CS: ${students.length}. `;
-        outputString += `List: ${students.toString()}\n`;
+        // let students = data.CS;
+        // outputString += `Number of students in CS: ${students.length}. `;
+        // outputString += `List: ${students.toString()}\n`;
 
-        students = data.SWE;
-        outputString += `Number of students in SWE: ${students.length}. `;
-        outputString += `List: ${students.toString()}\n`;
+        // students = data.SWE;
+        // outputString += `Number of students in SWE: ${students.length}. `;
+        // outputString += `List: ${students.toString()}\n`;
 
-        response.status(200).end(outputString);
+        // response.status(200).end(outputString);
+
+        // Apparently the way I did it does not play nice with the stupid checker's conditions
+        // Here is the solution they want
+        const students = [];
+        let message;
+        students.push('This is the list of our students');
+
+        for (const key of Object.keys(data)) {
+          message = `Number of students in ${key}: ${data[key].length}. List: ${data[key].join(', ')}`;
+          students.push(message);
+        }
+
+        response.status(200).end(`${students.join('\n')}`);
       })
       .catch((error) => {
         response.status(500).end(error.message);
